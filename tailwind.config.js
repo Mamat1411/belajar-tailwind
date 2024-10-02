26d287b0f73bc47eb18b87981640f7bf7a1daee8
Mamat1411 <muhammadmujahid14@gmail.com>
2024-10-02 20:55:02 +0700

    Animation: Default and Custom

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,16 @@ module.exports = {
       colors: {
         wpu: '#bada55',
         kopi: '#c0ffee'
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        'wiggle': 'wiggle 1s ease-in-out infinite'
+      },
+      keyframes: {
+        'wiggle': {
+          '0%, 100%' : {transform: 'rotate(-3deg)'},
+          '50%' : {transform: 'rotate(3deg)'}
+        }
       }
     },
   },
